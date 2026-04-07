bdebf4c41be5ec12d5c65f7b26b73f1f41adeb17
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-04-07 09:23:26 +0500

    fix: Project structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,13 @@
-// Lib Imports
 import type { Metadata } from 'next';
-
 import { Geist } from 'next/font/google';
 import { ThemeProvider } from '@/components/providers/theme-provider';
-
-// Asset Imports
 import './globals.css';
 
-// Component Imports
-import { Navbar } from '@/components/navbar';
-import { Footer } from '@/components/footer';
-
-// Fot Setup
 const Font_Geist = Geist({
   variable: '--font-geist',
   subsets: ['latin'],
 });
 
-// Metadata Setup
 export const metadata: Metadata = {
   title: {
     default: 'Urban Dish',
@@ -37,9 +27,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           enableSystem
           disableTransitionOnChange
         >
-          <Navbar />
-          <main>{children}</main>
-          <Footer />
+          {children}
         </ThemeProvider>
       </body>
     </html>
